5d90e95b035a25e02faeaad568de21bd9af4be41
Dean Asadi <eng.de86@gmail.com>
2018-12-19 17:46:52 -0600

    variables

--- src/javaBasic/Variables.java
+++ src/javaBasic/Variables.java
@@ -1,10 +1,15 @@
 package javaBasic;
 
 public class Variables {
-
+	// Java interpreter needs main method to run the program 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int // store integer numbers up to 32 bit
+		long // stores integer more than 32 bit
+		short  // Stores integers up to 32768 bit
+		String //Stores string values
+		
+		
 	}
 
 }
