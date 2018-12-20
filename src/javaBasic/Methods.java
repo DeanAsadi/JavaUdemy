b3b1abb7d3e82871cc5fd19d42999d7e0a0101ba
Dean Asadi <eng.de86@gmail.com>
2018-12-19 19:32:13 -0600

    Methods

--- src/javaBasic/Methods.java
+++ src/javaBasic/Methods.java
@@ -2,13 +2,23 @@ package javaBasic;
 
 public class Methods {
 	public static void main(String[] args) {
-		
+		//------------------------------------------------------------------------------
+
 		// We're invoking testMethod from MyUtils class
 		MyUtils.testMethod( " Passing arguments here :) " );// Calling Method here...
 		// Will call the testMethod( String str ) not testMethod( int integer ) 
-		
+		//------------------------------------------------------------------------------
+
 		MyUtils.addNums( 1, 2 ); // invoking addNums() from MyUtils class, into "main" method
 		System.out.println( 1 + 2 );
+		
+		//------------------------------------------------------------------------------
+		MyUtils.addString(" A Sting from addString method, in MyUtils class");
+		// We can capture the return in a variable, (We can only do that we we return int method.
+		String myStr = MyUtils.addString(" A Sting from addString method, in MyUtils class");
+		System.out.println(myStr);
+		//------------------------------------------------------------------------------
+
 	}
 	
 	
--- src/javaBasic/MyUtils.java
+++ src/javaBasic/MyUtils.java
@@ -11,5 +11,8 @@ public class MyUtils {
 	public static void addNums( int num1, int num2 ) {
 		
 	}
+	public static String  addString( String str ) { // To return you have to replace void with the DATA TYPE you're declaring.
+		return str;
+	}
 
 }
