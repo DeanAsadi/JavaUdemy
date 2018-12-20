82db27b8a78d2d8c5e7c1a545126420a481bab1e
Dean Asadi <eng.de86@gmail.com>
2018-12-19 18:07:53 -0600

    Array

---
+++ src/javaBasic/ArraysBasic.java
@@ -0,0 +1,15 @@
+package javaBasic;
+
+public class ArraysBasic {
+
+	public static void main(String[] args) {
+		
+		int[] value = new int[100]; // Values is TYPE int, stores 100 slots
+		value[0] = 1000; // store number 1000 in index [0]
+//		System.out.println(value);
+		
+		String[] words = new String[] { "This", "is", "test" };
+		System.out.println(words[2]);
+	}
+
+}
--- src/javaBasic/Variables.java
+++ src/javaBasic/Variables.java
@@ -1,19 +1,19 @@
-package javaBasic;
-
-public class Variables {
-	// Java interpreter needs main method to run the program 
-	public static void main(String[] args) {
-		
-		int // store integer numbers up to 32 bit
-		long // stores integer more than 32 bit
-		short  // Stores integers up to 32768 bit
-		double // Stores decimal variables
-		String //Stores string values
-		char // Stores single character 
-		boolean // Stores true or false values
-		
-		
-		
-	}
-
-}
+//package javaBasic;
+//
+//public class Variables {
+//	// Java interpreter needs main method to run the program 
+//	public static void main(String[] args) {
+//		
+//		int // store integer numbers up to 32 bit
+//		long // stores integer more than 32 bit
+//		short  // Stores integers up to 32768 bit
+//		double // Stores decimal variables
+//		String //Stores string values
+//		char // Stores single character 
+//		boolean // Stores true or false values
+//		// You can also create your own DATA TYPES
+//		
+//		
+//	}
+//
+//}
