a4a2d65a7e473a78b19104a7e6566d650e861447
Dean Asadi <eng.de86@gmail.com>
2018-12-19 19:19:54 -0600

    methods

--- src/javaBasic/Methods.java
+++ src/javaBasic/Methods.java
@@ -3,11 +3,13 @@ package javaBasic;
 public class Methods {
 	public static void main(String[] args) {
 		
-		testMethod( " Passing arguments here :) " );// Calling Method here...
+		// We're invoking testMethod from MyUtils class
+		MyUtils.testMethod( " Passing arguments here :) " );// Calling Method here...
+		// Will call the testMethod( String str ) not testMethod( int integer ) 
 		
+		MyUtils.addNums( 1, 2 ); // invoking addNums() from MyUtils class, into "main" method
+		System.out.println( 1 + 2 );
 	}
 	
-	public static void testMethod( String str ) {
-		System.out.println(" Printing a test,... " + str);
-	}
+	
 }
---
+++ src/javaBasic/MyUtils.java
@@ -0,0 +1,15 @@
+package javaBasic;
+
+public class MyUtils {
+	
+	public static void testMethod( String str ) {
+		System.out.println(" Printing a test, " + str);
+	}
+	public static void testMethod( int integer ) {
+		System.out.println(" Printing a test, " + integer);
+	}
+	public static void addNums( int num1, int num2 ) {
+		
+	}
+
+}
