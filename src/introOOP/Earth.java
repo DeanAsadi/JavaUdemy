e2a4a9d864d7ac4fc64a3f5de30d816c82665964
Dean Asadi <eng.de86@gmail.com>
2018-12-20 18:18:22 -0600

    Earth file

--- src/introOOP/Earth.java
+++ src/introOOP/Earth.java
@@ -4,7 +4,15 @@ public class Earth {
 
 	public static void main(String[] args) {
 		
-
+		Human tom;
+		tom = new Human();
+		
+		tom.name = "Tom";
+		tom.age = 32;
+		tom.height = 6;
+		tom.eyeColor = "Brown";
+		
+		tom.speak();
 	}
 
 }
