66ab05aa3ed90164fd318286f79dcb36aaac8382
Dean Asadi <eng.de86@gmail.com>
2018-12-21 13:55:45 -0600

    more String

--- src/javaBasic/Strings.java
+++ src/javaBasic/Strings.java
@@ -7,6 +7,7 @@ public class Strings {
 		
 	String str1 = "Hello";
 	String str2 = "There";
+	String str3 = "hello there programmer";
 	
 	if( str1.equals("Hello") ) { // user equals() to compare strings
 		System.out.println(true);
@@ -15,6 +16,12 @@ public class Strings {
 	if( str2.equalsIgnoreCase("THERE") ) { //user equalesIgnoreCase(); to ignore upper & lower cases characters.
 		System.out.println(true);
 	}
+	
+	System.out.println(str1.charAt(1)); // charAt();
+	
+	System.out.println(str3.indexOf("there")); // indexOf("there", 3); start searching for "there" starting from index 3
+	
+	System.out.println(str3.lastIndexOf("programmer")); // Starts from right to left
 
 	}
 
