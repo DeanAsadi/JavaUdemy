543f2fc74545da7115265411c99a6fb2b2107a7b
Dean Asadi <eng.de86@gmail.com>
2018-12-20 18:12:24 -0600

    Human class

--- src/introOOP/Human.java
+++ src/introOOP/Human.java
@@ -1,9 +1,40 @@
 package introOOP;
-// This class will allow us to create object of type Human.
+
 public class Human {
+	
+	String name;
+	int age;
+	int height;
+	String eyeColor;
+	
+	public Human() {
+		
+	}
+	
+	public void speak() {
+		
+		System.out.println(" Hi, my name is " + name);
+		System.out.println(" I am  " + age +  "  years old");
+		System.out.println(" and " + height + " tall" );
+		System.out.println(" also my eye color is " + eyeColor);
+
+
+	}
+	public void eat() {
+		System.out.println(" I like to eat Sushi");
+
+	}
+	public void walk() {
+		System.out.println(" and take a walk in the morning :) ");
+
+	}
 
 }
 
-// Classes, contain instruction for how the object should be created in, "how should behave in the application"
+
+
+
+//This class will allow us to create object of type Human.
+// Classes, contain instruction for "how should the object behave in the application"
 //constructor : Provides a way to construct objects
 //constructor : they alway's have the same name as your class they belong in.
