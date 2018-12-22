2f8d5a23c17e4b6317144771fb9306ccdda4a533
Dean Asadi <eng.de86@gmail.com>
2018-12-21 18:50:18 -0600

    exercise

--- src/javaBasic/Exercise.java
+++ src/javaBasic/Exercise.java
@@ -15,7 +15,22 @@ public class Exercise {
 	// extract all categories from the string argument
 	public static void printCategories(String str) {
 		
+		int count = 0;
+		
+		
+		while(true) {
+			int found = str.indexOf("category:", count);
+			if(found == -1) break;
+			int start = found + 9;
+			int end = str.indexOf(" ", start);
+			System.out.println(str.substring(start, end));
+			count +=  end;
+			
+		}
+		
 		
 	}
-
+	
 }
+
+
