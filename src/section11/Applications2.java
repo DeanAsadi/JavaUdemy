7d7cd122ea63fd029e54ce648586409e8b7954d5
Dean Asadi <eng.de86@gmail.com>
2018-12-30 11:20:58 -0600

    addAll()

---
+++ src/section11/Applications2.java
@@ -0,0 +1,27 @@
+package section11;
+
+import java.util.ArrayList;
+
+public class Applications2 {
+
+	public static void main(String[] args) {
+			
+		ArrayList<Integer> list1 = new ArrayList<Integer>();
+		
+		list1.add(10);
+		list1.add(15);
+		
+		
+		ArrayList<Integer> newList = new ArrayList<Integer>();
+		
+		newList.add(20);
+		newList.add(30);
+		
+		
+		list1.addAll(newList);
+		
+		// Add all values from ArrayList to another
+		System.out.println(list1);
+	}
+
+}
